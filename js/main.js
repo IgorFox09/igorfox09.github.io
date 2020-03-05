9a0c41bd6aa6150805bd9d71f0394659031ab209
IgorFox09 <60841907+IgorFox09@users.noreply.github.com>
2020-03-05 21:50:59 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.icon-menu').click(function(event){
+		$(this).toggleClass('active');
+		$(".header__menu-menu").toggleClass('active');
+		$("body").toggleClass('lock');
+	});	
+});
+
+///////SLIDER/////////
+
+
